15d90c784e3325b04f632e941bb046e4c691a23e
YungLust <artyr.yanchuk2@gmail.com>
2024-10-21 13:09:05 +0300

    JavaDoc generated by IDE

---
+++ lab3/JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"AudioDecorator"},{"p":"<Unnamed>","l":"FileDecorator"},{"p":"<Unnamed>","l":"ImageDecorator"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Message"},{"p":"<Unnamed>","l":"MessageDecorator"},{"p":"<Unnamed>","l":"TextMessage"}];updateSearchResults();
