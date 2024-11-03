6847a2b42d209044ca07b92c58cb6d8b22deffdd
YungLust <artyr.yanchuk2@gmail.com>
2024-11-03 22:27:50 +0200

    Added simple logic to print rectangles in terminal

--- lab4/src/DrawRectangle.java
+++ lab4/src/DrawRectangle.java
@@ -6,5 +6,25 @@ public class DrawRectangle {
         height = rectangle.getHeight();
         System.out.println("Drawing rectangle:");
 
+        for (int i = 0; i < height; i++) {
+            boolean heightSide = i == 0 || i == height - 1;
+            for (int j = 0; j < width; j++) {
+                boolean widthSide = j == 0 || j == width - 1;
+                if (widthSide && heightSide) {
+                    System.out.print("+");
+                }
+                else if (widthSide){
+                    System.out.print("|");
+                }
+                else if(heightSide){
+                    System.out.print("-—-");
+                }
+                else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+
     }
 }
