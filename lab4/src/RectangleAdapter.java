d771b8dc462aabcc10da777a171de630cddd3153
YungLust <artyr.yanchuk2@gmail.com>
2024-11-03 23:27:35 +0200

    Now it overrides getters, it is needed so DrawRectangle work properly. Now polygon radius converted to width and height correctly

--- lab4/src/RectangleAdapter.java
+++ lab4/src/RectangleAdapter.java
@@ -2,25 +2,40 @@
 public class RectangleAdapter extends Rectangle{
     private float width;
     private float height;
-    private RoundedRectangle roundedRectangle;
-    private Polygon polygon;
 
     public RectangleAdapter(RoundedRectangle roundedRectangle){
-        this.roundedRectangle = roundedRectangle;
 
-        //rounded rectangle is a rectangle when has 0 corner radius
+        //rounded rectangle is a rectangle when it has 0 corner radius
         roundedRectangle.setCornerRadius(0);
-        width = getWidth();
-        height = getHeight();
+
+        width = roundedRectangle.getWidth();
+        height = roundedRectangle.getHeight();
     }
 
     public RectangleAdapter(Polygon polygon){
-        this.polygon = polygon;
 
-        //polygon is a square when has 4 slides
-        polygon.setSlides(4);
+        //polygon is a square when it has 4 sides
+        polygon.setSides(4);
 
-        height = width = polygon.getRadius();
+        /*
+         * radius is distance from center of polygon to its points
+         * in our occasion it`s a half of the square`s diagonal
+         * so to get square`s side we need to multiply radius by sqrt(2)
+         */
+        float sqrSide = (float) (polygon.getRadius() * Math.sqrt(2));
+        width = sqrSide;
+        height = sqrSide;
+
+    }
+
+    @Override
+    public float getWidth(){
+        return width;
+    }
+
+    @Override
+    public float getHeight(){
+        return height;
     }
 }
 
