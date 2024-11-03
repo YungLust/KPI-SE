0c377ef6c2e8d13ed2c1546c7f4f83a41908028f
YungLust <artyr.yanchuk2@gmail.com>
2024-11-03 23:26:23 +0200

    Creates object of different figures adapts it and then draws

--- lab4/src/Main.java
+++ lab4/src/Main.java
@@ -1,4 +1,24 @@
 class Main{
     public static void main(String[] args){
+
+        //initialize our figues
+        RoundedRectangle roundedRect = new RoundedRectangle(4,8,25);
+        Polygon polygon = new Polygon(10, 12);
+        Rectangle rect = new Rectangle(12,6);
+
+        //adapt if needed
+        RectangleAdapter roundedAdapter = new RectangleAdapter(roundedRect);
+        RectangleAdapter polygonAdapter = new RectangleAdapter(polygon);
+
+        //draw figures
+        DrawRectangle drawRect = new DrawRectangle(roundedAdapter);
+        drawRect.draw();
+        System.out.println("\n");
+        drawRect.setRectangle(polygonAdapter);
+        drawRect.draw();
+        System.out.println("\n");
+        drawRect.setRectangle(rect);
+        drawRect.draw();
+
     }
 }
