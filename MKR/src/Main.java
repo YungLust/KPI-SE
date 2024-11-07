e18ba69cc1154b15f06167ab45d62b461ee0111d
YungLust <artyr.yanchuk2@gmail.com>
2024-11-07 11:45:53 +0200

    Update the creation of changed classes

--- MKR/src/Main.java
+++ MKR/src/Main.java
@@ -3,18 +3,20 @@ public class Main {
         Message msg = new TextMessage("Hi! I have some information for you.");
         System.out.println("*\n" + msg.send());
 
-        Message encryptedMsg = new EncryptionDecorator(msg, 4);
+        Message compressedMsg = new CompressionDecorator(msg,"_");
+        System.out.println("*\n" + compressedMsg.send());
+
+        Message encryptedMsg = new EncryptionDecorator(compressedMsg, 900);
         System.out.println("*\n" + encryptedMsg.send());
 
-        Message encryptedCompressedMsg = new CompressionDecorator(encryptedMsg);
-        System.out.println("*\n" + encryptedCompressedMsg.send());
+        Message authorMsg = new AuthorDecorator(encryptedMsg, "Artur Yanchuk");
+        System.out.println("*\n" + authorMsg.send());
 
-        Message encryptedCompressedAuthorMsg = new AuthorDecorator(encryptedCompressedMsg, "Artur Yanchuk");
-        System.out.println("*\n" + encryptedCompressedAuthorMsg.send());
+        Message dateTimeMsg = new DateTimeDecorator(authorMsg, "06.11.2024", "11:30");
+        System.out.println("*\n" + dateTimeMsg.send());
 
-        Message encryptedCompressedAuthorDateTimeMsg =
-                new DateTimeDecorator(encryptedCompressedAuthorMsg, "06.11.2024", "11:30");
-        System.out.println("*\n" + encryptedCompressedAuthorDateTimeMsg.send());
+        Message unEncryptedMsg = new EncryptionDecorator(dateTimeMsg, -900);
+        System.out.println("*\n" + unEncryptedMsg.send());
 
     }
 }
