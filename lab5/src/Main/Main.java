e85d869c0d622cabb0973530bdc6352b493c0a6e
YungLust <artyr.yanchuk2@gmail.com>
2024-11-18 22:54:18 +0200

    Added some prints and testing to unsubscribe methods

--- lab5/src/Main/Main.java
+++ lab5/src/Main/Main.java
@@ -13,23 +13,30 @@ public class Main {
 
         manager.createTask("Make another login variant", "Make login variant via Telegram", TaskCategory.BACKEND);
 
-        Task frontTask1 = manager.createTask("Change color button", "Make purhcase button blue in index.html", TaskCategory.FRONTEND);
+        Task frontTask1 = manager.createTask("Change color button", "Make purchase button blue in index.html", TaskCategory.FRONTEND);
         Task frontTask2 = manager.createTask("Recolor hero-section", "Change gradient in hero-section in index.html", TaskCategory.FRONTEND);
         User artur = new User("Artur");
         User ilya = new User("Ilya");
         User roma = new User("Roma");
 
+        System.out.println("\nSubscribing Artur to Fix bug\n*");
         BugTask.subscribe(artur);
         BugTask.changeState(TaskState.TESTING);
-        System.out.println();
+        System.out.println("\nSubscribing Ilya to Fix bug\n*");
         BugTask.subscribe(ilya);
         BugTask.changeState(TaskState.COMPLETED);
         System.out.println();
         manager.subscribe(TaskCategory.FRONTEND, roma);
-        Task frontTask3 = manager.createTask("Make button bigger", "Make confirm button bigger in checkout.html", TaskCategory.FRONTEND);
-        System.out.println();
+        System.out.println("Subscribing Roma to frontEnd\n*");
         frontTask1.changeState(TaskState.COMPLETED);
         frontTask2.changeState(TaskState.IN_PROGRESS);
+        Task frontTask3 = manager.createTask("Make button bigger", "Make confirm button bigger in checkout.html", TaskCategory.FRONTEND);
+        System.out.println("\nSubscribing Ilya to FrontEnd\nUnsubscribing Roma from FrontEnd\n*");
+        manager.subscribe(TaskCategory.FRONTEND,ilya);
+        manager.unsubscribe(TaskCategory.FRONTEND,roma);
+        frontTask2.changeState(TaskState.TESTING);
+        Task frontTask4 = manager.createTask("Make button bigger", "Make confirm button bigger in checkout.html", TaskCategory.FRONTEND);
+
 
     }
 }
