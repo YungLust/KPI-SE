d3c8e404296a6ef995feb806a10e98af69b8ed60
YungLust <artyr.yanchuk2@gmail.com>
2024-11-18 14:30:19 +0200

    Changed prints. When updating with category now shows new task

--- lab5/src/Main/Subscriber.java
+++ lab5/src/Main/Subscriber.java
@@ -1,7 +1,9 @@
 package Main;
 
 import TaskProccessing.Task;
+import TaskProccessing.TaskCategory;
 
 public interface Subscriber {
     void update(Task task);
+    void update(Task task, TaskCategory category);
 }
--- lab5/src/Main/User.java
+++ lab5/src/Main/User.java
@@ -2,6 +2,7 @@ package Main;
 
 import TaskProccessing.Task;
 import TaskProccessing.TaskCategory;
+import TaskProccessing.TaskState;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -11,10 +12,18 @@ public class User implements Subscriber{
     private Set<TaskCategory> categorySubs = new HashSet<>();
     private Set<Task> taskSubs = new HashSet<>();
 
+    public User(String name) {
+        this.name = name;
+    }
 
     @Override
     public void update(Task task) {
-        System.out.println(name+": ");
+        System.out.println(name+": new update");
         System.out.println(task.getTitle() + ": " + task.getState());
+    }
+    public void update(Task task, TaskCategory category){
+        System.out.println(name+": new update");
+        System.out.println("New task in: " + category);
+        System.out.println(task.toString());
     }
 }
