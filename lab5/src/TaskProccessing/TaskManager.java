f5d6910a8d79cabfa178ba666a07481fe94935e4
YungLust <artyr.yanchuk2@gmail.com>
2024-11-18 13:01:32 +0200

    When task created all subcribers of category notified

--- lab5/src/TaskProccessing/TaskManager.java
+++ lab5/src/TaskProccessing/TaskManager.java
@@ -9,17 +9,22 @@ import java.util.Set;
 import java.util.HashSet;
 
 public class TaskManager {
-    private Set<Task> tasks = new HashSet<>();
-    private HashMap<TaskCategory, List<Subscriber>> taskByCategory = new HashMap<>();
+    //private Set<Task> tasks = new HashSet<>();
+    private HashMap<TaskCategory, Set<Subscriber>> categorySubscribers = new HashMap<>();
+    private HashMap<TaskCategory, Set<Task>> taskByCategory = new HashMap<>();
     private TaskManager instance;
 
     private TaskManager() {
-        // fill HashSet with all the categories
+        // fill HashMaps with categories
         for (TaskCategory category : TaskCategory.values()) {
-            taskByCategory.put(category, new ArrayList<>());
+            categorySubscribers.put(category, new HashSet<>());
+        }
+        for (TaskCategory category : TaskCategory.values()){
+            taskByCategory.put(category, new HashSet<>());
         }
     }
 
+    //singleton
     public TaskManager getInstance() {
         if (instance == null) {
             instance = new TaskManager();
@@ -27,8 +32,18 @@ public class TaskManager {
         return instance;
     }
 
-    public void createTask(String title, String description, TaskCategory category) {
+    public Task createTask(String title, String description, TaskCategory category) {
         Task newTask = Task.createTask(title, description, category);
-        tasks.add(newTask);
+        //tasks.add(newTask);
+        Set<Subscriber> categorySubs = categorySubscribers.get(category);
+
+        //notify subscribers of the task`s category
+        for(Subscriber sub : categorySubs){
+            sub.update(category);
+        }
+
+        return newTask;
     }
+
+    public void subscribe
 }
