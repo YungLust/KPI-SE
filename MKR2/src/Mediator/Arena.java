6d343032b6d9ad023b3370aa8263fea4f6bda62d
YungLust <artyr.yanchuk2@gmail.com>
2024-12-19 23:31:33 +0200

    Now arena as a mediator has announcer too. It is used when some action on arena happens. Added methods to remove and add heroes to arena.

--- MKR2/src/Mediator/Arena.java
+++ MKR2/src/Mediator/Arena.java
@@ -1,15 +1,39 @@
 package Mediator;
 
 import Heroes.*;
+import Visitor.ArenaAnnouncer;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Arena implements  Mediator{
+public class Arena implements Mediator{
     private List<Hero> heroes = new ArrayList<>();
+    private ArenaAnnouncer announcer = new ArenaAnnouncer();
 
     public void addHero(Hero hero) {
+        // announce that new hero is in arena
+        announcer.notifySubs("New hero in arena: "+hero.getName());
+        //add new hero to subs
+        announcer.addSub(hero);
+        //add hero to arena
         heroes.add(hero);
-        System.out.println(hero.getName() + " has entered the arena.");
+    }
+
+    public void removeHero(Hero hero){
+        //announce that some hero is eliminated
+        announcer.notifySubs(hero.getName()+" got eliminated!");
+        //remove dead hero from subs
+        announcer.removeSub(hero);
+        //remove hero from arena
+        heroes.remove(hero);
+    }
+
+    public void attack(Hero hero, Hero enemy){
+        //attack enemy
+        hero.attack(enemy);
+        // notify attacker and enemy
+        enemy.update("You got attacked by "+hero.getName()+"Remaining health: "+enemy.getHp());
+        hero.update("You attack "+enemy.getName());
+
     }
 }
