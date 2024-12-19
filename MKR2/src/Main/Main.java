72c8d4e771ce73328e760c4ae1c56e7d2420f1ec
YungLust <artyr.yanchuk2@gmail.com>
2024-12-19 23:32:07 +0200

    Tested creation via factory and update method

--- MKR2/src/Main/Main.java
+++ MKR2/src/Main/Main.java
@@ -1,7 +1,15 @@
 package Main;
 
+import AbstractFactory.*;
+import Heroes.*;
+import Visitor.*;
+
 public class Main {
     public static void main(String[] args){
-
+        ArcherFactory archerFactory = new ArcherFactory();
+        WarriorFactory warriorFactory = new WarriorFactory();
+        MageFactory mageFactory = new MageFactory();
+        Hero mage = mageFactory.createHero();
+        mage.update("You just spawned!");
     }
 }
